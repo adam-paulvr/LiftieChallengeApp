619fb92144d224894ccdaa12eb0d3679b5d202b8
sethdbarr <seth.d.barrios@gmail.com>
2025-03-08 14:30:04 -0600

    Bind LiftView to CoreData obj via ObservedObject and update UI on change.
    Add button to undo check-in

--- Liftie Challenge/Liftie Challenge/LiftView.swift
+++ Liftie Challenge/Liftie Challenge/LiftView.swift
@@ -12,64 +12,121 @@ import UniformTypeIdentifiers
 
 
 struct LiftView: View {
-    var lift: LCLift
+    @ObservedObject var lift: LCLift
     @State private var isCameraPresented = false
     @State private var media: MediaType?
     
     var body: some View {
+        
+        // Lift title
+        Text(lift.name ?? "Electric chair")
+            .font(.title)
+            .padding(.leading)
+            .foregroundColor(lift.beerd ? .green : .red)
+            .frame(maxWidth: .infinity)
+            .padding(10)
+        
         VStack{
             
-            Text(lift.name ?? "Electric chair")
+            // Check-in status
+            ZStack{
+                Text(lift.beerd ? "CHUGGED" : "UNCHUGGED")
+                    .fontWeight(.bold)
+                    .font(.system(size: 45))
+                    .colorInvert()
+                    .padding(15)
+                    .shadow(radius: 10) // Flavortown
+                    .multilineTextAlignment(.center)
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 10) // Apply a rounded rectangle outline
+                    .stroke(Color.white, lineWidth: 5) // Set outline color and width
+            )
+            .padding(.top, 50)
+            .shadow(radius: 10) // Flavortown
+            .rotationEffect(.degrees(-7)) // Apply a 10-degree rotation
+    
             
-            if(lift.beerd){
-                Text("Chair has been Beerd")
-            }else{
-                Text("Chair has not been Beerd")
-            }
             VStack{
-                // Button to initiate recording
-                Button(action: {
-                    // Action for button tap
-                    print("Video incrimination tapped")
-                    isCameraPresented.toggle()
-                }) {
-                    Text("Chug")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 200, height: 200)
-                        .background(Color.red)
-                        .clipShape(Circle())
-                        .shadow(radius: 10)
+                if lift.beerd {
+                    if let media = media {
+                        switch media {
+                        case .photo(let image):
+                            // Display the photo if it's an image
+                            ImageCardView(image: image)
+                        case .video(let url):
+                            // Display the video URL
+                            Text("Video URL: \(url.absoluteString)")
+                        }
+                    }
+                    
+                    Spacer()
+                
+                    Button(action: {
+                        // Reset lift data
+                        lift.beerd = false
+                        // TODO: Update lift media directly.
+                        media = nil
+                    }) {
+                        Text("I lied. Unchug.")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, minHeight: 70)
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .shadow(radius: 5) // Flavortown
+                    }
+                    .padding(.horizontal, 20) // Add horizontal padding around the button
+                } else {
+                    // Button to initiate recording
+                    Button(action: {
+                        // Action for button tap
+                        print("Video incrimination tapped")
+                        isCameraPresented.toggle()
+                    }) {
+                        Text("Chug")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(lift.beerd ? .green : .red)
+                            .padding()
+                            .frame(width: 250, height: 250)
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                    }
+                    .padding()
                 }
-                .padding()
             }
+            .padding(.top)
             .fullScreenCover(isPresented: $isCameraPresented) {
                 CameraPicker(isPresented: $isCameraPresented) { media in
                     self.media = media
+                    lift.beerd = true
                 }
             }
         }
-        
-        /**START-DELETE: Placeholder proving media is returned **/
-        
-        // Handling and displaying the returned media
-        if let media = media {
-            switch media {
-            case .photo(let image):
-                // Display the photo if it's an image
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .background(lift.beerd ? Color.green : Color.red)
+        .animation(.easeInOut(duration: 0.3), value: lift.beerd)
+    }
+}
+
+// For showing a square image.
+struct ImageCardView: View {
+    @State private var cardWidth: CGFloat = 0
+    let image: UIImage
+
+    var body: some View {
+        Rectangle()
+            .aspectRatio(1, contentMode: .fit)
+            .overlay(
                 Image(uiImage: image)
                     .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-            case .video(let url):
-                // Display the video URL
-                Text("Video URL: \(url.absoluteString)")
-            }
-        }
-        
-        /**END-DELETE: Placeholder proving media is returned **/
+                    .scaledToFill()
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .shadow(radius: 10)
+            .padding(20)
     }
 }
 
